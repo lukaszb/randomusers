cf33084258cf8645e71e5858a669933532fcff8d
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2017-05-16 18:56:38 +0200

    allow to change param attrs

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
   // add your custom rules here
   'rules': {
     'no-console': 0,
+    'no-param-reassign': 0,
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       'js': 'never',
