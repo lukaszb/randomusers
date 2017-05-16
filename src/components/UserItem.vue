5a7091111e48881dbc9f25ee8181dadb05f29bc4
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2017-05-16 19:38:08 +0200

    show more user info

--- src/components/UserItem.vue
+++ src/components/UserItem.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <h3>{{ user.email }}</h3>
+  <div class="columns">
+    <div class="column is-8 is-offset-2">
+
+      <div class="card user-card">
+
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <img :src="user.picture.medium" alt="Image"/>
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">{{ user.name.title }} {{ user.name.first }} {{ user.name.last }}</p>
+              <p class="subtitle is-6">{{ user.email }}</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,3 +29,10 @@ export default {
   props: ['user'],
 };
 </script>
+
+<style>
+.image img {
+  border: 2px solid #efe;
+  border-radius: 50%;
+}
+</style>
