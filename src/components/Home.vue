8d3fd7d33c4a698c471d7af47a04677128cd0e45
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2017-05-16 18:36:13 +0200

    fetch random users

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,36 @@
 <template>
   <div class="homepage">
     <h1>Randomusers</h1>
+
+    <user-list :users="users"></user-list>
+
+    <button @click="fetchMoreUsers()">Fetch more users</button>
   </div>
 </template>
 
 <script>
+import UserList from '@/components/UserList';
+import axios from 'axios';
+
+
 export default {
+  components: { UserList },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    async fetchMoreUsers() {
+      const count = 10;
+      const url = `https://randomuser.me/api/?&nat=us,gb,fr,de&results=${count}`;
+      try {
+        const response = await axios.get(url);
+        this.users = this.users.concat(response.data.results);
+      } catch (error) {
+        console.error('Something went wrong');
+      }
+    },
+  },
 };
 </script>
---
+++ src/components/UserList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="user-list">
+    <div v-for="user in users">
+      <h3>{{ user.email }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    users: { type: Array, default: [] },
+  },
+};
+</script>
