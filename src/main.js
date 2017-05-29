3815559043e2b0e67f0c48d276264f4bc82f7eed
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2017-05-29 21:49:57 +0200

    added rx packages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRx from 'vue-rx';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
 import App from './App';
 import router from './router';
 import store from './store';
@@ -9,6 +13,12 @@ import { capfirst } from './utils';
 Vue.config.productionTip = false;
 
 Vue.filter('capfirst', capfirst);
+
+Vue.use(VueRx, {
+  Observable,
+  Subscription,
+  Subject,
+});
 
 /* eslint-disable no-new */
 new Vue({
